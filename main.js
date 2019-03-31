49e68631f0ad43aff33f7382ec787e5c8d11d913
Erusebiru <Erusebiru@gmail.com>
2019-03-31 16:33:10 +0200

    goal available

--- main.js
+++ main.js
@@ -51,53 +51,43 @@ function moveDown(player){
     }
 }
 
-function getPositions(element){
+/*function getPositions(element){
     var position = element.position();
     var width = element.width();
     var height = element.height();
 
     return [[position.left, position.left + width], [position.top, position.top + height]];
-}
+}*/
 
-function comparePositions(ball,player){
-    var r1 = player.position().left < ball.position().left + player.outerWidth(true) - 5 ? true : false;
+function stopBall(ball,player,direction){
+    if(direction === "right"){
+        var r1 = player.position().left < ball.position().left + player.outerWidth(true) - 5 ? true : false;
+        var end = ball.position().left >= $('.box').width() ? true : false;
+        
+    }else{
+        var r1 = player.position().left > ball.position().left - player.outerWidth(true) ? true : false;
+        var end = ball.position().left < 0 ? true : false;
+    }
+
     var r2 = player.position().top > ball.position().top ? true : false;
     var r3 = player.position().top + player.outerHeight(true) < ball.position().top ? true : false;
-    
-    if(r1){
-        if(r3){
-            return true;
-        }else if(r2){
-            return true;
+
+    if(end){
+        return "fin";
+    }else{
+        if(r1){
+            if(r3){
+                return true;
+            }else if(r2){
+                return true;
+            }else{
+                return false;
+            }
         }else{
-            return false;
+            return true;
         }
-    }else{
-        return true;
     }
-}
-
-function stopBall(ball,player){
-    var ballPos = getPositions(ball);
-    var playerPos = getPositions(player);
-    console.log(comparePositions(ball,player));
-
-    return true;
-    /*if((player.position().top < ball.position().top 
-        && player.position().top + player.outerHeight(true) > ball.position().top) 
-        && player.position().left > ball.position().left + player.outerWidth(true)){
-        return true;
-    }else{
-        return false;
-    }*/
-}
-
-function stopBallLeft(ball,player){
-    if((player.position().top < ball.position().top && player.position().top + player.outerHeight(true) > ball.position().top) && player.position().left < ball.position().left - player.outerWidth(true)){
-        return true;
-    }else{
-        return false;
-    }
+    
 }
 
 function startGame(ball,player1,player2){
@@ -106,15 +96,24 @@ function startGame(ball,player1,player2){
     var moving = setInterval(function(){
         
         if(direction === "right"){
+            let game = stopBall(ball,player2,direction);
             ball.css({'left':ball.position().left+5})
-            if(comparePositions(ball,player2)){
+            if(game === true){
                 
-            }else{
+            }else if(game === "fin"){
+                $('.result.left').text(parseInt($('.result.left').text())+1);
+                clearInterval(moving)
+            }  else{
                 direction = "left";
             }
         }else if(direction === "left"){
-            if(stopBallLeft(ball,player1)){
-                ball.css({'left':ball.position().left-5})
+            let game = stopBall(ball,player1,direction);
+            ball.css({'left':ball.position().left-5})
+            if(game === true){
+            
+            }else if(game === "fin"){
+                $('.result.right').text(parseInt($('.result.right').text())+1);
+                clearInterval(moving)
             }else{
                 direction = "right";
             }
@@ -132,14 +131,12 @@ $(document).on('keypress',function(e){
             startGame(ball,player1,player2);
             break;
         case 119: 
-            $('.result.left').text(parseInt($('.result.left').text())+1);
             moveUp(player1); 
             break;
         case 115: 
             moveDown(player1); 
             break;
         case 56:
-            $('.result.right').text(parseInt($('.result.right').text())+1);
             moveUp(player2);
             break;
         case 50:
